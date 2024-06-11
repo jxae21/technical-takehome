39045b8f246b5119912c1df92ed94c317a06af30
Joshua Kravitz <jlkravitz@gmail.com>
2024-06-11 11:03:42 -0400

    Initial commit

---
+++ q1/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Counter: <!-- Put counter here --></h1>
+        <button>Increment</button>
+        <button>Decrement</button>
+        <button>Reset</button>
+    </div>
+</template>
+
+<!-- Uncomment to use JavaScript instead of TypeScript. -->
+<!-- <script> -->
+<!-- If you'd like to use the options API, remove the 'setup' keyword. -->
+<script setup lang="ts">
+/*
+    Refer to the following Vue.js documentation to implement the functionality:
+
+    - Refs for reactive variables: https://vuejs.org/guide/essentials/reactivity-fundamentals.html#ref
+    - Handling Events: https://vuejs.org/guide/essentials/event-handling.html#inline-handlers
+
+    Suggested steps:
+    1. Define a reactive variable for the count using ref.
+    2. Implement methods to increment, decrement, and reset the count.
+    3. Use the @click directive to call these methods when the buttons are clicked.
+*/
+</script>
